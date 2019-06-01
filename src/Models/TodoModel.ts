75f8d540a5b7eebbccbf95f97bcd6d22decd492d
Your Name <you@example.com>
2019-06-01 11:26:23 -0400

    build

--- src/Models/TodoModel.ts
+++
@@ -1,9 +0,0 @@
-export class TodoModel {
-    id: number;
-    title: string;
-    done: boolean = false;
-    constructor(title: string) {
-        this.title = title;
-        this.id = Math.random();
-    }
-}
